26e05686ae9bfe6289da853b56d1beb011672a69
Lalit <lalitsonawane@gmail.com>
2016-12-04 07:52:30 +0530

    Initial commit

---
+++ app/src/main/java/com/lalitsonawane/chatting/chatterpatter/CodelabPreferences.java
@@ -0,0 +1,8 @@
+package com.lalitsonawane.chatting.chatterpatter;
+
+public class CodelabPreferences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
